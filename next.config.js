0df963ae14c1c7abf603e853f4106b8b01b68c06
unknown <shravan.sagara@gmail.com>
2024-03-03 01:55:52 +0530

    Added config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    serverRuntimeConfig: {
+      // Define your custom hostname and port here
+      HOST: 'http://0.0.0.0',
+      PORT: process.env.PORT || '3000',
+    },
+  };
