f0ac7fa04419708b481287d61b84be90b41e2d73
bitmos <shravan.sagara@gmail.com>
2024-03-02 14:45:21 +0530

    added channel page

---
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.js
@@ -0,0 +1,43 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { initProfile } from "@/lib/init-profile";
+import { ChatHeader } from "@/components/chat/ChatHeader";
+import { db } from "@/lib/database";
+
+const ChannelIdPage = async ({ params }) => {
+  const profile = await initProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
+};
+
+export default ChannelIdPage;
