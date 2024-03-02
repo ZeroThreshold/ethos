f7508e9a7e7c163f29da3b35297d3702aff3ac80
Vinay Mulugund <roverflow5@gmail.com>
2024-03-02 13:12:57 +0530

    added mobile menu

---
+++ components/MobileToggle.js
@@ -0,0 +1,21 @@
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { NavigationSidebar } from "./navigation/NavSidebar";
+import ServerSidebar from "./server/ServerSidebar";
+
+export const MobileToggle = ({ serverId }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
