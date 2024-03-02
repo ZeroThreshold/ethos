348d03e70664bb995dce8bb7635f36153840d110
RakshithJKashyap <rakshithj07@gmail.com>
2024-03-02 12:46:35 +0530

    added server sidebar components

---
+++ components/server/ServerHeader.js
@@ -0,0 +1,91 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useModelStore } from "@/hooks/useModelStore";
+
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+const ServerHeader = ({ server, role }) => {
+  const { onOpen } = useModelStore();
+
+  const isAdmin = role === "ADMIN";
+  const isModerator = isAdmin || role === "MOD";
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none" asChild>
+          <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+            {server.name}
+            <ChevronDown className="h-5 w-5 ml-auto" />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+          {isModerator && (
+            <DropdownMenuItem
+              onClick={() => onOpen("invite", { server })}
+              className="text-blue-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+            >
+              Invite People
+              <UserPlus className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isAdmin && (
+            <DropdownMenuItem
+              onClick={() => onOpen("members", { server })}
+              className="px-3 py-2 text-sm cursor-pointer"
+            >
+              Manage Members
+              <Users className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isModerator && (
+            <DropdownMenuItem
+              onClick={() => onOpen("create-channel", { server })}
+              className="px-3 py-2 text-sm cursor-pointer"
+            >
+              Create Channel
+              <PlusCircle className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {isModerator && <DropdownMenuSeparator />}
+          {isAdmin && (
+            <DropdownMenuItem
+              className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+              onClick={() => onOpen("delete-server", { server })}
+            >
+              Delete Group
+              <Trash className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+          {!isAdmin && (
+            <DropdownMenuItem
+              className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+              onClick={() => onOpen("leave-server", { server })}
+            >
+              Leave Group
+              <LogOut className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default ServerHeader;
---
+++ components/server/ServerSidebar.js
@@ -0,0 +1,133 @@
+import { redirect } from "next/navigation";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+
+import { ScrollArea } from "../ui/scroll-area";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/database";
+import { currentProfile } from "@/lib/currentProfile" 
+import ServerHeader from "./ServerHeader";
+import { ServerSection } from "./ServerSection";
+import { ServerChannel } from "./ServerChannel";
+import { ServerMember } from "./ServerMember";
+
+const iconMap = {
+  ["TEXT"]: <Hash className="mr-2 h-4 w-4" />,
+  ["VOICE"]: <Mic className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  ["USER"]: null,
+  ["MOD"]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+  ["ADMIN"]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />,
+};
+
+const ServerSidebar = async ({ serverId }) => {
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === "TEXT"
+  );
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === "VOICE"
+  );
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile.id
+  );
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const role = server.members.find(
+    (member) => member.profileId === profile.id
+  )?.role;
+
+  return (
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1  px-3">
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={"TEXT"}
+              role={role}
+              label="Text Channels"
+            />
+            <div className="space-y-[2px]">
+              {textChannels.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  role={role}
+                  server={server}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {!!audioChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={"VOICE"}
+              role={role}
+              label="Colab Channels"
+            />
+            <div className="space-y-[2px]">
+              {audioChannels.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  role={role}
+                  server={server}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {!!members?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="members"
+              role={role}
+              label="Members"
+              server={server}
+            />
+            <div className="space-y-[2px]">
+              {members.map((member) => (
+                <ServerMember key={member.id} member={member} server={server} />
+              ))}
+            </div>
+          </div>
+        )}
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default ServerSidebar;
