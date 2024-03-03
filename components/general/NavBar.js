d9e2449c1ef25da6640462c1f6e2f0f8d5bdf197
Vinay Mulugund <roverflow5@gmail.com>
2024-03-03 15:17:59 +0530

    logos

--- components/general/NavBar.js
+++ components/general/NavBar.js
@@ -1,4 +1,7 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+
 import Link from "next/link";
 
 export const GradientText = ({ text, gradient, classes }) => {
@@ -17,9 +20,10 @@ export const GradientText = ({ text, gradient, classes }) => {
 };
 
 const NavBar = () => {
+  const router = usePathname();
   return (
     <div className="w-full flex justify-between py-4 px-6 border-b shadow-sm">
-      <Link href={`/`}>
+      <Link href={router === "/" ? "/home" : "/"}>
         <GradientText
           text="ETHOS"
           gradient="-webkit-linear-gradient(45deg, #338cf5, #4fd1c5)"
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 const nextConfig = {
-    images: {
-      domains: ['jyothyit.ac.in', 'www.iiits.ac.in', 'vidyaniketan.edu.in', 'rvpucollege.edu.in'],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: [
+      "jyothyit.ac.in",
+      "www.iiits.ac.in",
+      "vidyaniketan.edu.in",
+      "rvpucollege.edu.in",
+      "www.ryangroup.org",
+    ],
+  },
+};
+
+export default nextConfig;
